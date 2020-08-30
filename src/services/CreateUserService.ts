feb12a48eb14492fb1f90549e70764673c5bc388
Luiz Bonatto <luizfelipebonatto@hotmail.com>
2020-08-30 20:21:11 -0300

    Users Authenticaion Inicialized

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,38 @@
+import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
+
+import User from '../models/User';
+
+interface Request {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    const ckeckUserExist = await usersRepository.findOne({
+      where: { email },
+    });
+
+    if (ckeckUserExist) {
+      throw new Error('Email Adress Already Used.');
+    }
+
+    const hashedPassword = await hash(password, 8);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
